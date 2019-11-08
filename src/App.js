2d7911a3153b549bd6874468a336d35d23405595
Matt Wood <matt@cratebind.com>
2019-11-08 09:56:30 -0600

    Add final styles, add refactored button and reset button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+function Button({ children, onClick }) {
+  return (
+    <button
+      className="counter-button"
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  )
+}
+
 function App() {
+  const [count, setCount] = useState(10);
+
   return (
     <div className="App">
-      Hello World!
+      <Button
+        onClick={() => setCount(count - 1)}
+      >
+        -
+      </Button>
+      <div className="counter-value">
+        {count}
+      </div>
+      <Button
+        onClick={() => setCount(count + 1)}
+      >
+        +
+      </Button>
+      <Button
+        onClick={() => setCount(0)}
+      >
+        Reset
+      </Button>
     </div>
   );
 }
